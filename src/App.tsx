958b55823c4533f5c437668245865d56a038b9d1
Eugen <vasilenkoevgen88@gmail.com>
2023-07-13 23:06:10 +0300

    Children | React.FC<PropsType>

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import React, {useState} from 'react';
+import React, {Suspense, useState} from 'react';
 import './App.css';
-
-
-
-
+import {SuperButton} from './Component/SuperButton';
+import {SuperTodoList} from './Component/SuperTodoList';
 
 function App() {
+
+    const tasks =[
+        {id: 1, title:"HTMLCSS", isDone:true},
+        {id: 2, title:"ls", isDone:true},
+        {id: 3, title:"React", isDone:true}
+    ]
     return (
-        <div></div>
+        <div>
+            <SuperButton callBack={()=>{}} color={'red'}>First SuperButton</SuperButton>
+            <SuperButton callBack={()=>{}} color={'secondary'}>Second SuperButton</SuperButton>
+            <SuperButton callBack={()=>{}} disabled >Third SuperButton</SuperButton>
+            <SuperTodoList tasks={tasks} >
+                <SuperButton callBack={()=>{}} color={'red'}>First SuperButton</SuperButton>
+                <SuperButton callBack={()=>{}} color={'secondary'}>Second SuperButton</SuperButton>
+                <SuperButton callBack={()=>{}} disabled >Third SuperButton</SuperButton>
+                <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+
+            </SuperTodoList>
+        </div>
     );
 }
-
-
 
 export default App;
